a74550f3f31c67afa6d892efe4c6e4fa87a5d561
xu-hn <350152004@qq.com>
2020-05-28 18:25:55 +0800

    fixed

--- baymax_ui_auto_test/common/login_who.py
+++ baymax_ui_auto_test/common/login_who.py
@@ -1,14 +1,9 @@
 def who_login(who):
     login = {
-        'Dam': '../YAML/user_for_test/user_dir1_for_dam.yaml',
-        'bayMax': '../YAML/user_for_test/user_dir1.yaml',
-        'Beiruan': '../YAML/user_for_test/user_dir1_for_beiruan.yaml',
-        'Cab': '../YAML/user_for_test/user_dir1.yaml',
-        'Cad': '../YAML/user_for_test/user_dir1.yaml',
-        'YinPao': '../YAML/user_for_test/user_dir1_for_beiruan.yaml',
-        'ShuBo': '../YAML/user_for_test/user_dir1.yaml'
+        #'Dam': '../YAML/user_for_test/user_dir1_for_dam.yaml',
+        'THDax': '../YAML/user_for_test/user_dir1_for_THDax'
     }
     return login[who]
 
 if __name__ == "__main__":
-    who_login('bayMax')
+    who_login('THDax')
